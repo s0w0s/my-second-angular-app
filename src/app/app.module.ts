d6764e1f29e7be9b0586a331611f8e72b977136b
woosung <jangwooeng97@gmail.ocm>
2021-12-17 01:19:57 +0900

    feat(module): ngmodule사용법 및 기존 내장 디랙티프 ngif, ngfor ngClass등 사용한 연습 코드 작성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
